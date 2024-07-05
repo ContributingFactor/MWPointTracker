9b6719a72050f58985b65f8150fcca64761bbb2a
Marco Carra <cfactorprints@gmail.com>
2024-07-05 09:21:50 -0700

    Updated to work on .cn site as well.

--- scripts/content.js
+++ scripts/content.js
@@ -106,7 +106,7 @@ function countDownloads() {
         if (textColor) {
             badge.style.cssText = `color:${textColor}`;
         }
-        badge.textContent = `P:${total}`
+        badge.textContent = `P: ${total}`
         printsDiv.insertAdjacentElement("afterend", badgeDiv);
         badgeDiv.insertAdjacentElement("beforeend", badge);
     }
